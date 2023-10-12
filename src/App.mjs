f7d53bc9bb75e5acfe801e69f08ed5bb7d26292b
Dyutee Moy <dyuteemoy.de@gmail.com>
2023-10-12 19:45:48 +0530

    NEW change on 7th sept file7

--- src/App.mjs
+++ src/App.mjs
@@ -43,10 +43,10 @@ app.use('/api', routerPincode);
 app.use( express.static( path.join( __dirname, 'Uploads/Profile' )));
 app.use( express.static( path.join( __dirname, 'Uploads/Products' )));
 
-// app.use( express.static( "./dist"));
-// app.get("*",(req,res)=>{
-//     res.sendFile(path.resolve(__dirname,"dist","index.js"))
-// })
+app.use( express.static( "./dist"));
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"dist","index.js"))
+})
 
 
 export default httpServer;
