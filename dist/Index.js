fb574bc011eb9029250d5315cefe067a23bcc0f5
Dyutee Moy <dyuteemoy.de@gmail.com>
2023-10-12 19:20:49 +0530

    NEW change on 7th sept file5

--- dist/Index.js
+++ dist/Index.js
@@ -6,7 +6,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 
 _App["default"].listen(process.env.APP_PORT, function () {
   return console.log('Server on port ' + process.env.APP_PORT);
-}); // const server = http.listen(process.env.APP_PORT, () => {
+});
+ // const server = http.listen(process.env.APP_PORT, () => {
 //     const address = server.address();
 //     const host = address.address === '::' ? 'localhost' : address.address;
 //     const port = address.port;
