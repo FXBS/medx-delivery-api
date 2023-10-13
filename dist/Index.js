1861fee8d0c185e07dbab45d92b2958acd4086a3
Dyutee Moy <dyuteemoy.de@gmail.com>
2023-10-13 17:23:44 +0530

    NEW change on 7th sept file 18

--- dist/Index.js
+++ dist/Index.js
@@ -2,14 +2,11 @@
 
 var _App = _interopRequireDefault(require("./App.js"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-_App["default"].listen(process.env.APP_PORT, function () {
-  return console.log('Server on port ' + process.env.APP_PORT);
+// http.listen( process.env.APP_PORT, () => console.log('Server on port ' + process.env.APP_PORT));
+
+var server = _App["default"].listen(process.env.APP_PORT, function () {
+  var address = server.address();
+  var host = address.address === '::' ? 'localhost' : address.address;
+  var port = address.port;
+  console.log("Server is running at http://".concat(host, ":").concat(port));
 });
-
-// const server = http.listen(process.env.APP_PORT, () => {
-//     const address = server.address();
-//     const host = address.address === '::' ? 'localhost' : address.address;
-//     const port = address.port;
-
-//     console.log(`Server is running at http://${host}:${port}`);
-//   });
--- src/Index.js
+++ src/Index.js
@@ -1,13 +1,13 @@
 import http from './App.js';
 
 
-http.listen( process.env.APP_PORT, () => console.log('Server on port ' + process.env.APP_PORT));
+// http.listen( process.env.APP_PORT, () => console.log('Server on port ' + process.env.APP_PORT));
 
 
-// const server = http.listen(process.env.APP_PORT, () => {
-//     const address = server.address();
-//     const host = address.address === '::' ? 'localhost' : address.address;
-//     const port = address.port;
+const server = http.listen(process.env.APP_PORT, () => {
+    const address = server.address();
+    const host = address.address === '::' ? 'localhost' : address.address;
+    const port = address.port;
   
-//     console.log(`Server is running at http://${host}:${port}`);
-//   });
+    console.log(`Server is running at http://${host}:${port}`);
+  });
