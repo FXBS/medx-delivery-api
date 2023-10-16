d03c717d048b632608bae66cef474d857dceaa5e
Dyutee Moy <dyuteemoy.de@gmail.com>
2023-10-16 15:44:48 +0530

    NEW change on 7th sept file made changes to order controller and order routes

--- src/Controller/OrdersController.js
+++ src/Controller/OrdersController.js
@@ -8,7 +8,29 @@ export const addNewOrders = async (req, res = response ) => {
 
         const { uidAddress, total, typePayment,  products } = req.body;
 
-        const orderdb = await pool.query('INSERT INTO orders (client_id, address_id, amount, pay_type) VALUES (?,?,?,?)', [ req.uid, uidAddress, total, typePayment ]);
+        const addressData = await pool.query('SELECT Latitude, Longitude FROM addresses WHERE id = ?', [uidAddress]);
+
+        if (addressData.length === 0) {
+            return res.status(400).json({
+                resp: false,
+                msg : 'Invalid address ID'
+            });
+        }
+
+        const latitude = addressData[0].Latitude;
+        const longitude = addressData[0].Longitude;
+
+        // Log data before inserting into the orders table
+        console.log('Data before inserting into orders table:', {
+            client_id: req.uid,
+            address_id: uidAddress,
+            latitude,
+            longitude,
+            amount: total,
+            pay_type: typePayment
+        });
+
+        const orderdb = await pool.query('INSERT INTO orders (client_id, address_id, latitude, longitude, amount, pay_type) VALUES (?,?,?,?,?,?)', [ req.uid, uidAddress,latitude, longitude, total, typePayment ]);
 
         products.forEach(o => {
              pool.query('INSERT INTO orderDetails (order_id, product_id, quantity, price) VALUES (?,?,?,?)', [ orderdb.insertId, o.uidProduct, o.quantity, o.quantity * o.price ]);
@@ -27,6 +49,7 @@ export const addNewOrders = async (req, res = response ) => {
     }
 
 }
+
 
 export const getOrdersByStatus = async (req, res = response ) => {
 
@@ -76,7 +99,7 @@ export const updateStatusToDispatched = async ( req, res = response ) => {
 
         const { idDelivery, idOrder } = req.body;
 
-        await pool.query('UPDATE orders SET status = ?, delivery_id = ? WHERE id = ?', [ 'DISPATCHED', idDelivery, idOrder ]);
+        await pool.query('UPDATE orders SET status = ?, delivery_id = ? WHERE id = ?', [ 'DISPATCHED TO PARTNER', idDelivery, idOrder ]);
 
         res.json({
             resp: true,
@@ -91,6 +114,31 @@ export const updateStatusToDispatched = async ( req, res = response ) => {
     }
 
 }
+
+
+export const updateStatusToAgent = async ( req, res = response ) => {
+
+    try {
+
+        const { idDelivery, idOrder } = req.body;
+
+        await pool.query('UPDATE orders SET status = ?, delivery_id = ? WHERE id = ?', [ 'ASSIGNED TO AGENT', idDelivery, idOrder ]);
+
+        res.json({
+            resp: true,
+            msg : 'Order DISPATCHED TO AGENT'
+        });
+        
+    } catch (e) {
+        return res.status(500).json({
+            resp: false,
+            msg : e
+        });
+    }
+
+}
+
+
 
 export const getOrdersByDelivery = async ( req, res = response ) => {
 
--- src/Router/Order.routes.js
+++ src/Router/Order.routes.js
@@ -10,6 +10,7 @@ router.post('/add-new-orders', verifyToken, orders.addNewOrders );
 router.get('/get-orders-by-status/:statusOrder', verifyToken, orders.getOrdersByStatus );
 router.get('/get-details-order-by-id/:idOrderDetails', verifyToken, orders.getDetailsOrderById );
 router.put('/update-status-order-dispatched', verifyToken, orders.updateStatusToDispatched );
+router.put('/update-status-order-agent', verifyToken, orders.updateStatusToAgent );
 router.get('/get-all-orders-by-delivery/:statusOrder', verifyToken, orders.getOrdersByDelivery );
 router.put('/update-status-order-on-way/:idOrder', verifyToken, orders.updateStatusToOntheWay );
 router.put('/update-status-order-delivered/:idOrder', verifyToken, orders.updateStatusToDelivered );
