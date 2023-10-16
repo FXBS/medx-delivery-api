1a886afc75ef512c6b602e9e2f89c735488a8e7e
Dyutee Moy <dyuteemoy.de@gmail.com>
2023-10-16 15:12:37 +0530

    NEW change on 7th sept file made changes to deliverycontroller, logincontroller, categoryroute

--- src/Controller/DeliveryController.js
+++ src/Controller/DeliveryController.js
@@ -21,3 +21,65 @@ export const getAllDelivery = async ( req, res = response ) => {
     }
 
 }
+
+
+export const getAllDeliveryAgentsForPartner = async ( req, res = response ) => {
+
+    try {
+
+          const { partnerId } = req.query;
+      console.log('PartnerId :', partnerId);
+
+      
+
+        const deliverydb = await pool.query(`CALL SP_ALL_DELIVERY_AGENTS_FOR_PARTNER(?);`,[partnerId]);
+
+        
+              console.log('Query Result:', deliverydb);
+
+        // Check the structure of the queryResult and log individual rows if needed
+        if (deliverydb && deliverydb[0] && deliverydb[0][0]) {
+        const rows = deliverydb[0][0];
+        console.log('Query Rows:', rows);
+        }
+
+
+        res.json({
+            resp: true,
+            msg : 'Get All Delivery Agents For a Partner',
+            delivery: deliverydb[0] 
+        });
+        
+    } catch (e) {
+
+        console.error('Error:', e); // Log the error for debugging
+        return res.status(500).json({
+            resp: false,
+            msg : e
+        });
+    }
+
+}
+
+
+export const getAllDeliveryPartner = async ( req, res = response ) => {
+
+    try {
+
+        let deliverydb = await pool.query(`CALL SP_ALL_DELIVERY_PARTNER();`);
+
+        res.json({
+            resp: true,
+            msg : 'Get All Delivery Partner',
+            delivery: deliverydb[0] 
+        });
+        
+    } catch (e) {
+        return res.status(500).json({
+            resp: false,
+            msg : e
+        });
+    }
+
+}
+
--- src/Router/Category.routes.js
+++ src/Router/Category.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import * as category from '../Controller/CategoryController.js';
-import { getAllDelivery } from '../Controller/DeliveryController.js';
+import { getAllDelivery, getAllDeliveryPartner, getAllDeliveryAgentsForPartner  } from '../Controller/DeliveryController.js';
 import { verifyToken } from '../Middleware/ValidateToken.js';
 
 const router = Router();
@@ -9,5 +9,7 @@ const router = Router();
 router.post('/add-categories', verifyToken, category.addCategories);
 router.get('/get-all-categories', verifyToken, category.getAllCategories );
 router.get('/get-all-delivery', verifyToken, getAllDelivery);
+router.get('/get-all-delivery-agents-for-partner', verifyToken, getAllDeliveryAgentsForPartner);
+router.get('/get-all-delivery-partner', verifyToken, getAllDeliveryPartner);
 
 export default router;
